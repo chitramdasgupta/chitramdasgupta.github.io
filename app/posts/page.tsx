cf5f418b12b09eebea3edca13395da1b243e817f
Chitram Dasgupta <chitramdasgupta@gmail.com>
2024-07-06 18:50:02 +0530

    Revert "Moving over to Next.js!"
    This reverts commit d14376ab39b6bdadb2986c324073e56d163b3e29.

--- app/posts/page.tsx
+++
@@ -1,5 +0,0 @@
-import Link from "next/link";
-
-export default function Posts() {
-  return <h1>Posts</h1>;
-}
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/chitramdasgupta.github.io",
-  assetPrefix: "/chitramdasgupta.github.io/",
-  distDir: "docs",
-};
-
-export default nextConfig;
